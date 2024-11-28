0094dc57c900ae4df104dd8267dea434c7b4b241
Noskina Olha <noskinaolga1@gmail.com>
2024-11-28 13:42:14 +0100

    Added MailJS integration for contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,38 @@
 "use client";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function ContactPage() {
   const text = "Say Hello";
 
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        () => {
+          setError(true);
+        }
+      );
+  };
 
   return (
     <motion.div
@@ -38,17 +64,23 @@ export default function ContactPage() {
         </div>
 
         {/* Form Container  */}
-        <form className="h-auto lg:h-full w-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-6 sm:p-12 lg:p-24">
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="h-auto lg:h-full w-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-6 sm:p-12 lg:p-24"
+        >
           <span className="text-lg sm:text-xl lg:text-2xl">
             Dear OlyaPla Dev,
           </span>
           <textarea
             rows={1}
+            name="user_message"
             placeholder="Share your thoughts here..."
-            className="bg-transparent resize-none border-b-2 border-black outline-none mt-2 pb-12 focus:border-violet-500 transition-colors text-base sm:text-lg w-full"
+            className="bg-transparent resize-none border-b-2  border-black outline-none mt-2 pb-14  focus:border-violet-500 transition-colors text-base sm:text-lg w-full"
           ></textarea>
           <span className="text-lg sm:text-xl">Email for response:</span>
           <input
+            name="user_email"
             type="email"
             placeholder="example@mail.com"
             className="bg-transparent border-b-2 border-black outline-none mt-2 pb-4 focus:border-violet-500 transition-colors text-base sm:text-lg w-full"
@@ -59,6 +91,17 @@ export default function ContactPage() {
           <button className="bg-purple-200 hover:bg-violet-300 rounded font-semibold text-gray-600 p-4sm:p-4 lg:p-6 text-base sm:text-lg transition-colors">
             Send 🚀
           </button>
+
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
         </form>
       </div>
     </motion.div>
